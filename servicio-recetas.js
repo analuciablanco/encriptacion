3fff2d7b79cdeef51707a4d234d2ccd0d2d82aba
Ana Lucia Blanco <analuciablanco@outlook.com>
2021-02-09 18:11:31 -0700

    mas cosas

--- servicio-recetas.js
+++ servicio-recetas.js
@@ -1,33 +1,55 @@
-const { request } = require("https");
 
-const server = require("fastify")();
+//npm install fastify
+
+const fs = require("fs");
+
+const server = require("fastify")({
+    https: {
+        key: fs.readFileSync(__dirname + "/tls/llave-privada.key"),
+        cert: fs.readFileSync(__dirname + "/shared/tls/certificado-publico.cert")
+    }
+});
 
 const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 4000;
 
-console.log(`Proceso pid =${process.pid}`);
 
+console.log(`Proceso pid=${process.pid}`);
+
+//http://localhost:4000/recetas/42  <- (id)
 server.get('/recetas/:id', async (request, response) => {
-    console.log(`Proceso de atencion de solicitud pid=${process.pid}`);
+
+    console.log(`Proceso de atención de la solicitud pid=${process.pid}`);
     const id = Number(request.params.id);
+
     if (id !== 42) {
+
         response.statusCode = 404;
-        return { error : "No se encontró la receta" };
+        return { error : "No se encontró receta" };
+
     }
-    return {
-        pid : process.pid,
+
+    return { 
+        pid : process.pid, 
         receta : {
             id,
-            nombre: "Tacos de pollo",
-            pasos: "Agarras la tortlla y le pones el pollo",
-            ingradientes: [
+            nombre : "Taco de pollo",
+            pasos : "Agarra la tortilla y echale pollo",
+            ingredientes : [
+
                 {id: 1, nombre: "Tortilla", cantidad: "2 unidades"},
-                {id: 2, nombre: "Pollo", cantidad: "80 grs"}
+                {id: 2, nombre: "Pollo", cantidad: "800 grs"}
+
             ]
         }
     };
+
 });
 
-server.listen(PORT, HOST, () =>{
+server.listen(PORT, HOST, () => { 
+
     console.log(`Servidor ejecutandose en http://${HOST}:${PORT}`);
+
 });
+
+
